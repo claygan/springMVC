8ba11f36ad4f79328f8def91dc12b8b1cd5e9ca6
ganshimin <ganshimin@zhongzhihui.com>
2017-05-04 21:21:17 +0800

    远程hassian调用

--- src/main/java/com/quest/controllers/MyClassController.java
+++ src/main/java/com/quest/controllers/MyClassController.java
@@ -1,5 +1,6 @@
 package com.quest.controllers;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,17 +9,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.quest.commons.ServiceFactory;
+import com.quest.entity.WordItem;
 import com.quest.pojos.Student;
+import com.quest.service.HelloRemoteService;
 
 @Controller
 @RequestMapping("/myclass")
 public class MyClassController {
+	
+	@Resource
+	HelloRemoteService helloRemoteService;
 	
 	@RequestMapping("/student")
 	public String queryStudent(HttpServletRequest request, HttpServletResponse response, Model model){
 		
 		Student student = ServiceFactory.getBean("student", Student.class);
 		model.addAttribute("studentInfo", student.study());
-		return "student";
+		return "myclass/student";
+	}
+	
+	@RequestMapping("/words")
+	public String queryWords(HttpServletRequest request,Model model){
+		helloRemoteService.saySomething("我是一只，小小的石头！");
+		
+		WordItem wordItem = helloRemoteService.listenWords("我是一只小蜜蜂", "嗡嗡嗡");
+		System.out.println("words: "+wordItem.getWords());
+		System.out.println("sound: "+wordItem.getSound());
+		model.addAttribute("wordItem", wordItem);
+		return "myclass/words";
 	}
 }
